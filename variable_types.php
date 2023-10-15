94aa4a442f6e7bcc1512bc04cae64af181ca1b5b
Nicole Bay-an <113017884+nicolebay-an@users.noreply.github.com>
2023-10-15 20:06:04 +0800

    Create variable_types.php

---
+++ variable_types.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$int_var = 12345;
+$another_int = -12345 + 12345;
+
+if (TRUE)
+ print("This will always print<br>");
+else
+ print("This will never print<br>");
+?>
+</body>
+</html>
